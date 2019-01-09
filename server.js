87d7d18b70a4bc75cbcdabefa9b562a97b859340
Odilio <odilionoronha@gmail.com>
2019-01-09 13:48:36 -0200

    Refatoração do server

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var passport = require('passport');
 var Strategy = require('passport-local').Strategy;
+const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn();
+//import { default as User } from "../models/User";
 var db = require('./db');
 
 
@@ -47,8 +49,6 @@ passport.deserializeUser(function(id, cb) {
 });
 
 
-
-
 // Create a new Express application.
 var app = express();
 
@@ -57,7 +57,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 // Use application-level middleware for common functionality, including
-// logging, parsing, and session handling.
+// parsing, and session handling.
 app.use(require('cookie-parser')());
 app.use(require('body-parser').urlencoded({ extended: true }));
 app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
@@ -68,19 +68,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // Define routes.
-app.get('/',
-  function(req, res) {
+app.get('/',(req, res)=> {
     res.render('home', { user: req.user });
   });
 
-app.get('/login',
-  function(req, res){
+app.get('/login',(req, res)=>{
     res.render('login');
   });
   
 app.post('/login', 
-  passport.authenticate('local', { failureRedirect: '/login' }),
-  function(req, res) {
+  passport.authenticate('local', { failureRedirect: '/login' }),(req, res) =>{
     res.redirect('/');
   });
   
@@ -91,9 +88,7 @@ app.get('/logout',
   });
 
 //A user must be logged in before accessing this page.
-app.get('/profile',
-  require('connect-ensure-login').ensureLoggedIn(),
-  function(req, res){
+app.get('/profile',ensureLoggedIn,(req, res)=>{
     res.render('profile', { user: req.user });
   });
 
